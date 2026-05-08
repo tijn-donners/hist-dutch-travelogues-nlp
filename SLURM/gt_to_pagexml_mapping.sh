f46b630d840adf1474aa110f80fafb0aa92ebc2f
Tijn Donners <contact@tijndonners.nl>
2026-05-08 13:16:35 +0200

    script fix habrok

--- SLURM/gt_to_pagexml_mapping.sh
+++ SLURM/gt_to_pagexml_mapping.sh
@@ -10,15 +10,14 @@
 #SBATCH --time=04:00:00
 #SBATCH --gpus-per-node=1
 
-module load ollama/0.6.0-GCCcore-12.3.0
-module load Python/3.11.3-GCCcore-12.3.0
+module load ollama/0.6.0-GCCcore-13.3.0-CUDA-12.6.0
+module load Python/3.11.3-GCCcore-13.3.0
 
-# Save models to scratch, not home
 export OLLAMA_MODELS=/scratch/$USER/ollama_models_scratch
 
 # Start Ollama server in background
-ollama serve &
-sleep 15  # wait for server to start
+ollama serve
+sleep 5  # wait for server to start
 
 source $HOME/venvs/ollama/bin/activate
 pip install pandas ollama
