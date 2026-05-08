9d5075dc45c5584716b3031e05ad89c681c74e9c
Tijn Donners <contact@tijndonners.nl>
2026-05-08 12:20:50 +0200

    slurm added for habrok

---
+++ SLURM/gt_to_pagexml_mapping.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=gt_pagexml_mapping
+#SBATCH --output=gt_%j.log
+#SBATCH --error=gt_%j.err
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --time=04:00:00
+#SBATCH --gpus-per-node=1
+
+module load ollama/0.6.0-GCCcore-12.3.0
+module load Python/3.11.3-GCCcore-12.3.0
+
+source $HOME/venvs/ollama/bin/activate
+pip install pandas ollama
+
+python3 /scratch/s6437265/hist-dutch-travelogues-nlp/gt_to_pagexml.py
