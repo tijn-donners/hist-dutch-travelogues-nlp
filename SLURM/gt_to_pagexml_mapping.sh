86a86885ec1681bccaead13514ee5e70bf5e147d
Tijn Donners <contact@tijndonners.nl>
2026-05-08 12:28:10 +0200

    final edits slurm job

--- SLURM/gt_to_pagexml_mapping.sh
+++ SLURM/gt_to_pagexml_mapping.sh
@@ -13,6 +13,13 @@
 module load ollama/0.6.0-GCCcore-12.3.0
 module load Python/3.11.3-GCCcore-12.3.0
 
+# Save models to scratch, not home
+export OLLAMA_MODELS=/scratch/$USER/ollama_models
+
+# Start Ollama server in background
+ollama serve &
+sleep 15  # wait for server to start
+
 source $HOME/venvs/ollama/bin/activate
 pip install pandas ollama
 
