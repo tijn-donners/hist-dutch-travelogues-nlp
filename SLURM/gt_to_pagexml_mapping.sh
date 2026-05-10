52b88faf87bd0d4368334303a5b720c79deab089
Tijn Donners <contact@tijndonners.nl>
2026-05-10 10:29:45 +0200

    removed ollama pull and pip installs

--- SLURM/gt_to_pagexml_mapping.sh
+++ SLURM/gt_to_pagexml_mapping.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=48G
+#SBATCH --mem=32G
 #SBATCH --time=10:00:00
 #SBATCH --gpus-per-node=1
 
@@ -15,14 +15,15 @@ export OLLAMA_MODELS=/scratch/$USER/ollama_models_scratch
 # Start Ollama (use the binary rather than the module, since it is very outdated)server in background
 /scratch/$USER/ollama/bin/ollama serve &
 sleep 15  # wait for server to start
-/scratch/$USER/ollama/bin/ollama pull gemma4:31b
+#/scratch/$USER/ollama/bin/ollama pull gemma4:31b
 /scratch/$USER/ollama/bin/ollama ls
 
 echo "Loading model into GPU memory..."
 /scratch/$USER/ollama/bin/ollama run gemma4:31b "hello" --nowordwrap
+/scratch/$USER/ollama/bin/ollama ps
 echo "Model loaded, starting Python script..."
 
 source $HOME/venvs/ollama/bin/activate
-pip install pandas ollama
+#pip install pandas ollama
 
 python3 -u /scratch/s6437265/hist-dutch-travelogues-nlp/gt_to_pagexml.py
